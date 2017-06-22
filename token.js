8873c1f408784f944061e1e25bb1c1e6f6fe3e89
HowIsKyrgAlreadyTaken <thorawayfun@gmail.com>
2017-06-22 18:54:41 +0200

    test

---
+++ token.js
@@ -0,0 +1,13 @@
+module.exports = {
+  token: 'MzE4NzQyNzI1NzM2Mzk4ODU5.DA20Pw.kL25WoysopuoY42-wGMZ1HsfFAI',
+  cn: {
+    host: 'ec2-54-217-222-254.eu-west-1.compute.amazonaws.com',
+    port: 5432,
+    database: 'd2bfpj5hakq75n',
+    user: 'irisoovcfanwqm',
+    password: '56d8ee1d6e9886b0aa973ee2c7aca08aa73417e13413a457660f8f5a50cdda9b',
+    ssl: 'require'
+  },
+  url: 'postgres://irisoovcfanwqm:56d8ee1d6e9886b0aa973ee2c7aca08aa73417e13413a457660f8f5a50cdda9b@ec2-54-217-222-254.eu-west-1.compute.amazonaws.com:5432/d2bfpj5hakq75n?ssl=true'
+};
+
